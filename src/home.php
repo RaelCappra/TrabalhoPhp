7d074853c489679bf573ef14e6179d1c58cd5f0c
RaelCappra <rael.cappra@gmail.com>
2015-05-12 13:23:01 -0300

    adicionada interface basica de selecao de mes em home (ainda nao eh buscado por mes)

--- src/home.php
+++ src/home.php
@@ -5,6 +5,12 @@ $_SESSION['usuario'] = "nome";
 
 $movimentacoes = getListMovimentacao();
 
+$meses = Array("Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", 
+				"Julho", "Agost", "Setembro", "Outubro", "Novembro", "Dezembro", "Todos os meses");
+if (!isset($_GET['mes'])){
+	$_GET['mes'] = count($meses) - 1;
+}
+
 /*
 $movimentacoes = Array(
 	Array("100", "bar", "foobar", "parrot", 0),
@@ -22,15 +28,35 @@ $movimentacoes = Array(
 	<link rel="stylesheet" type="text/css" href="../css/style.css"/>
 </head>
 <body>
+	Mês
+	<form method="GET">
+		<select name="mes" onchange="this.form.submit()">
+		<?php
+			
+			for($mes = 0; $mes < count($meses); $mes++){
+				$selected = ($mes == $_GET['mes'] ? "selected" : "");
+				echo(
+				"<option value=\"$mes\" " . $selected . ">". 
+				$meses[$mes] .			
+				"</option>"
+			 	);
+			} 
+		?>
+		</select>
+	</form>
 	<table>
 	<?php
-		foreach ($movimentacoes as $mov){
-			$tipo = $mov['tipo'] ? "receita" : "despesa";
-			$valor = $mov['valor'];
-			$categoria = $mov['categoria'];
-			$data = $mov['data'];
-			$descricao = $mov['descricao'];
-			echo("<tr class=$tipo> <td>$valor</td><td>$categoria</td><td>$data</td><td>$descricao</td> </tr>");
+		if(!$movimentacoes){
+			echo "Você não possui movimentações registradas neste mês";
+		} else {
+			foreach ($movimentacoes as $mov){
+				$tipo = $mov['tipo'] ? "receita" : "despesa";
+				$valor = $mov['valor'];
+				$categoria = $mov['categoria'];
+				$data = $mov['data'];
+				$descricao = $mov['descricao'];
+				echo("<tr class=$tipo> <td>$valor</td><td>$categoria</td><td>$data</td><td>$descricao</td> </tr>");
+			}
 		}
 	?>
 	</table>
