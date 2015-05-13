e541eeed997a35a4e661529063ee6f4a4bb6c5ae
RaelCappra <rael.cappra@gmail.com>
2015-05-13 14:33:39 -0300

    movido logout.php e login.php e lib para src (FIXED)

---
+++ src/login.php
@@ -0,0 +1,28 @@
+<?php
+	$username = $_POST['username'];
+	$senha = $_POST['senha'];
+	include "../lib/conexao.php";
+	$conexao = getConnection();
+	if($conexao){
+		//echo("Conectado");
+	}else{
+		echo("Nao foi possivel se conectar ao banco");
+		die();
+	}
+
+	$sql = "select * from usuario where username = $1";
+	$resultado = pg_query_params($conexao, $sql, Array($username));
+	$usuario = pg_fetch_array($resultado);
+	if(!$usuario or $usuario['senha'] != $senha){
+		session_start();
+		session_unset();
+		
+		$_SESSION['login_error'] = 1;
+		header('location:index.php');
+	} else{
+		session_start();
+		$_SESSION['login_error'] = 0;
+		$_SESSION['usuario'] = $username;
+		header('location:home.php');
+	}
+?>
